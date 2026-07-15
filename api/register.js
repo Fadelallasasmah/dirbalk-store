b9f7ce0e23699d9141c2428f45368e0d6b1b8816
Fadelallasasmah <hamzafadel828@gmail.com>
2026-07-15 23:20:05 +0300

    Update register.js

--- api/register.js
+++ api/register.js
@@ -22,6 +22,7 @@ export default async function handler(req, res) {
     const payload = {
       name: (req.body?.name || '').toString(),
       age: (req.body?.age || '').toString(),
+      gender: (req.body?.gender || '').toString(),
       phone: (req.body?.phone || '').toString(),
       email: (req.body?.email || '').toString(),
       ip,
